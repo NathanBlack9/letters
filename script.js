2722725012a3af4752f92de7c29d7b0fe239d573
Suren <suren.khachatryan1999@bk.ru>
2022-03-19 18:21:17 +0300

    новая верстка + хедер

--- script.js
+++ script.js
@@ -48,5 +48,24 @@ function changeCursor() {
     });
   });
 }
-
 changeCursor();
+
+
+$(document).ready( () => {
+  const $this = $('.area__left');
+
+  $this.addClass('_animated');
+  // const $this = $('.area__left>img');
+  // const $defaultPos = $this.css('right');
+
+  // // повторить с интервалом 1 секунда
+  // let timerId = setInterval(() =>  {
+  //   let $pos = $defaultPos;
+  //   $this.clone().appendTo($parent).css('right', '+='+ $pos +'')
+  // }, 1000);
+
+  // // остановить вывод через 3 секунд
+  // setTimeout(() => { clearInterval(timerId); }, 3000);
+});
+
+ 
